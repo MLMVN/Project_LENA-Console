f21d6dc453cf0d1c94fa0a2128c0993c4cac9834
MatchingMole <katoo52zeke@gmail.com>
2014-09-24 02:11:33 -0500

    Initial

---
+++ CSharpArrayTest/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+// using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Numerics;
+using BitMiracle.LibTiff.Classic;
+
+namespace CSharpArrayTest
+{
+    public class Program
+    {
+        // main
+        static int Main(string[] args)
+        {
+            //blah
+            // objects used in "using" scope will be disposed immediately after exiting the scope
+            // Attempt to read an image
+            Tiff image = Tiff.Open("Lena_Y.tif", "r");
+            // obtain basic tag information of the image
+            #region GetTagInfo
+            int width = image.GetField(TiffTag.IMAGEWIDTH)[0].ToInt();
+            int height = image.GetField(TiffTag.IMAGELENGTH)[0].ToInt();
+            byte bits = image.GetField(TiffTag.BITSPERSAMPLE)[0].ToByte();
+            byte pixel = image.GetField(TiffTag.SAMPLESPERPIXEL)[0].ToByte();
+            double dpiX = image.GetField(TiffTag.XRESOLUTION)[0].ToDouble();
+            double dpiY = image.GetField(TiffTag.YRESOLUTION)[0].ToDouble();
+            #endregion
+            /*
+            // display information
+            System.Console.WriteLine("Width is : {0}\nHeight is: {1}\ndpi is: {2}\nThe scanline is {3}\nBits per Sample is: {4}", width, height, dpiX, image.ScanlineSize(), bits);
+            Console.WriteLine("Sample per pixel is: {0}", pixel);
+
+            // store the intensity values of the image to 2d array
+            byte[,] clean = new byte[height, width];
+            clean = TiffFunctions.Tiff2Array(image, height, width);
+            */
+            byte[,] clean = TiffFunctions.Tiff2Array(image,height,width);
+            Tiff noisyImage = Tiff.Open("Lena_Y_gauss_0.1.tif", "r");
+            byte[,] noisy = new byte[height, width];
+            noisy = TiffFunctions.Tiff2Array(noisyImage,height,width);
+
+            //TiffFunctions.LearnSetPatch(clean, noisy, 9, 400, "SamplePatchGeneration.txt");
+
+            //byte[,] denoised = MLMVN.Activation(noisy, 3, "Lena_Y_gauss_0.1_kernel_3_N_3_54_S_500_SD_2.8.wgt", 384, 3, 54);
+            //TiffFunctions.WriteToFile(denoised, width, height, bits, pixel, dpiX, dpiY);
+
+            //int[] networkSize = new int[4] { 511, 511, 511, 121 };
+            int[] networkSize = new int[4] { 511, 511, 511, 169 };
+            //int[] inputsPerSample = new int[4] { 170, 512, 512, 512 };
+            //string fileName = "Lena_Y_gauss_0.1_samples_400_patch_11_rmse_4.0.wgt";
+            //byte[,] denoised = MLMVN.fdenoiseNeural2(noisy, 3, fileName, 4, networkSize, 384);
+            //TiffFunctions.WriteToFile(denoised, width, height, bits, pixel, dpiX, dpiY);
+
+            string fileNameS = "Lena_Y_Gauss_0.1_Samples_300_Patches_13x13.txt";
+            //string fileNameW = "Lena_Y_gauss_0.1_rmse_3.0_session_2.wgt";
+            //int[,] output = MLMVN.MLMVN_TEST(fileNameS, 200, fileNameW, 4, networkSize, inputsPerSample, 384);
+            //Complex[][,] weights = MLMVN.MLMVN_Learning(fileNameS, 200, fileNameW, 4, networkSize, inputsPerSample, 5.0, 0.0, 384, true);
+
+            string fileName = "testWeightSave.wgt";
+            Complex[][,] weights = MLMVN.MLMVN_Learning(fileNameS, 200, " ", 4, networkSize, 300.0, 0.0, 384, true);
+            MLMVN.saveMlmvnWeights(fileName, weights, networkSize);
+            return 0;
+        } // end main
+    } // end class
+} // end namespace 
---
+++ CSharpArrayTest/TiffFunctions.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BitMiracle.LibTiff.Classic;
+
+namespace CSharpArrayTest
+{
+    public class TiffFunctions
+    {
+        // convert tiff image to 2d byte array
+        public static byte[,] Tiff2Array(Tiff image, int height, int width)
+        {
+            // store the image information in 2d byte array
+            // reserve memory for storing the size of 1 line
+            byte[] scanline = new byte[image.ScanlineSize()];
+            // reserve memory for the size of image
+            byte[,] im = new byte[height, width];
+            // grab the intensity values
+            for (int i = 0; i < height; i++)
+            {
+                image.ReadScanline(scanline, i);
+                for (int j = 0; j < width; j++)
+                    im[i, j] = scanline[j];
+            } // end grabbing intensity values
+            return im;
+        } // end method
+
+        // write the image to file
+        public static void WriteToFile(byte[,] im, int width, int height, byte bits, byte pixel, double dpiX, double dpiY)
+        {
+            // Attempt to recreate the image from 2d byte array im
+            using (Tiff output = Tiff.Open("processed.tif", "w"))
+            {
+                // simple error check
+                if (output == null)
+                {
+                    Console.WriteLine("Can not read the image.");
+                    return;
+                }
+                // set tag information
+                #region SetTagInfo
+                output.SetField(TiffTag.IMAGEWIDTH, width);
+                output.SetField(TiffTag.IMAGELENGTH, height);
+                output.SetField(TiffTag.BITSPERSAMPLE, bits);
+                output.SetField(TiffTag.SAMPLESPERPIXEL, pixel);
+                output.SetField(TiffTag.ORIENTATION, Orientation.TOPLEFT);
+                output.SetField(TiffTag.PHOTOMETRIC, Photometric.MINISBLACK);
+                output.SetField(TiffTag.PLANARCONFIG, PlanarConfig.CONTIG);
+                output.SetField(TiffTag.ROWSPERSTRIP, height);
+                output.SetField(TiffTag.XRESOLUTION, dpiX);
+                output.SetField(TiffTag.YRESOLUTION, dpiY);
+                output.SetField(TiffTag.RESOLUTIONUNIT, ResUnit.CENTIMETER);
+                output.SetField(TiffTag.COMPRESSION, Compression.NONE);
+                output.SetField(TiffTag.FILLORDER, FillOrder.MSB2LSB);
+                #endregion
+
+                // reserve buffer
+                byte[] buffer = new byte[width * sizeof(byte /*can be changed depending on the format of the image*/)];
+                // obtain each line of the final byte arrays and write them to a file
+                for (int i = 0; i < height; i++)
+                {
+                    for (int k = 0; k < width; k++)
+                    {
+                        buffer[k] = im[i, k];
+                    }
+                    // write
+                    output.WriteScanline(buffer, i);
+                }
+                // write to file
+                output.WriteDirectory();
+            } // end inner using
+        }
+
+        // create surrounding borders
+        public static byte[,] MirrorImage(byte[,] im, int height, int width, int offset)
+        {
+            // write code here... someday
+            // reserve 2darray with extended sizes
+            int newHeight = height + (offset * 2);
+            int newWidth = width + (offset * 2);
+            byte[,] image = new byte[newHeight, newWidth];
+            Console.WriteLine("Calling MirrorImage...\nSize of new matrix is {0} by {1}", newWidth, newHeight);
+            // copy original image
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    image[i + offset, j + offset] = im[i, j];
+                }
+            }
+            // copy columns - confirmed to work
+            for (int i = 0; i < offset; i++) // 0~2
+            {
+                for (int row = offset; row < offset + height; row++) // 3~514
+                {
+                    // copy left columns
+                    image[row, i] = im[row - offset, offset - i];
+                    // copy right columns
+                    image[row, i + width + offset] = im[row - offset, width - i - 2];
+                } // end for
+            } // end for
+
+            for (int i = 0; i < offset; i++)
+            {
+                for (int col = 0; col < width + (offset * 2); col++)
+                {
+                    // copy top rows
+                    image[i, col] = image[(offset * 2) - i, col];
+                    // copy bottom rows
+                    image[i + height + offset, col] = image[height + offset - 2 - i, col];
+                }
+            }
+            return image;
+        }
+
+        // create kernel window
+        public static byte[,] CreateWindow(byte[,] im, int row, int col, int kernel, int offset)
+        {
+            byte[,] image = new byte[kernel, kernel];
+            for (int i = 0; i < kernel; i++)
+            {
+                for (int j = 0; j < kernel; j++)
+                {
+                    image[i, j] = im[row - offset + i, col - offset + j];
+                }
+            }
+            return image;
+        }
+
+        // create kernel window
+        public static byte[,] CreatePatch(byte[,] im, int row, int col, int kernel)
+        {
+            byte[,] array = new byte[kernel, kernel];
+            for (int i = 0; i < kernel; i++)
+            {
+                for (int j = 0; j < kernel; j++)
+                {
+                    array[i, j] = im[row + i, col + j];
+                }
+            }
+            return array;
+        }
+        public static byte[] CreatePatchAsArray(byte[,] im, int row, int col, int kernel)
+        {
+            byte[] array = new byte[kernel * kernel];
+            for (int i = 0; i < kernel; i++)
+            {
+                for (int j = 0; j < kernel; j++)
+                {
+                    array[(i * kernel) + j] = im[row + i, col + j];
+                }
+            }
+            return array;
+        }
+        // create learning samples
+        public static void LearnSet(byte[,] clean, byte[,] noised, int kernel, int offset, int sSize)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter("Sample_1.txt", true))
+            {
+                // get height and width
+                int cleanHeight = clean.GetLength(0);
+                int cleanWidth = clean.GetLength(1);
+                // write the number of samples to file
+                file.WriteLine(sSize.ToString());
+                // initialize random number generator
+                Random random = new Random();
+                // begin generating samples
+                for (int v = 0; v < sSize; v++)
+                {
+                    // generate random coordinate
+                    int randomRow = random.Next(0, 256);
+                    int randomCol = random.Next(0, 256);
+                    // fetch kernel
+                    byte[,] inputArray = TiffFunctions.CreateWindow(noised, randomRow + offset, randomCol + offset, kernel, offset);
+                    // get optimal intensity value from clean image
+                    int pixel = clean[randomRow, randomCol];
+                    // reserve byte array
+                    byte[] S = new byte[kernel * kernel];
+                    // transform multi dimensional inputArray to 1d array
+                    for (int i = 0; i < kernel; i++)
+                    {
+                        for (int j = 0; j < kernel; j++)
+                        {
+                            S[kernel * i + j] = inputArray[i, j];
+                        } // end for loop
+                    } // end for loop
+
+                    // Create custome code for GUI version to save it under specified user directory with preferred name
+                    // write to file
+                    for (int i = 0; i < S.Length; i++)
+                    {
+                        file.Write(S[i] + " ");
+                    }
+                    file.Write(pixel);
+                    file.WriteLine();
+                } // end for loop
+            } // end using scope
+        }
+
+        public static void LearnSetPatch(byte[,] clean, byte[,] noised, int kernel, int Size, string FileName)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(FileName, true)) // Samples.txt
+            {
+                // get height and width
+                int patchLength = kernel * kernel;
+                int twoPatchLength = patchLength * 2;
+                int rowSize = clean.GetLength(0);  //rowSize
+                int colSize = clean.GetLength(1);   //colSize
+                double temp = rowSize / kernel;
+                int sSizeRow = (int)Math.Floor(temp) - 1;
+                temp = colSize / kernel;
+                int sSizeCol = (int)Math.Floor(temp) - 1;
+                int sSize = sSizeRow * sSizeCol;
+                int sSizeRowIndex = sSizeRow * kernel;
+                int sSizeColIndex = sSizeCol * kernel;
+
+                // initialize variables to store patches
+                byte[] noisyVector = new byte[kernel * kernel];
+                byte[] cleanVector = new byte[kernel * kernel];
+                byte[,] vectorMAP = new byte[sSize, twoPatchLength];
+                Random random = new Random();
+
+                // generate all possible patches first
+                for (int row = 0; row < sSizeRow; row++)
+                {
+                    for (int col = 0; col < sSizeCol; col++)
+                    {
+                        //create patch from noisy image
+                        noisyVector = CreatePatchAsArray(noised, row * kernel, col * kernel, kernel);
+                        cleanVector = CreatePatchAsArray(clean, row * kernel, col * kernel, kernel);
+                        // store in vectorMap
+                        // noisy first
+                        for (int v = 0; v < patchLength; v++)
+                            vectorMAP[row * sSizeRow + col, v] = noisyVector[v];
+                        // then clean
+                        for (int v = patchLength; v < twoPatchLength; v++)
+                            vectorMAP[row * sSizeRow + col, v] = cleanVector[v - patchLength];
+                    }
+                }
+                byte[] S = new byte[twoPatchLength];
+                // done generating all possible patches.
+                // Now we need to randomly select patches
+                for (int v = 0; v < Size; v++)
+                {
+                    //generate random coordinate
+                    int randomRow = random.Next(0, sSize);
+                    //Create custome code for GUI version to save it under specified user directory with preferred name
+                    //write to file
+                    for (int i = 0; i < twoPatchLength; i++)
+                        S[i] = vectorMAP[randomRow, i];
+                    for (int i = 0; i < twoPatchLength; i++)
+                    {
+                        //file.Write(vectorMAP[randomRow, i]);
+                        file.Write(S[i] + " ");
+                    }
+                    file.WriteLine();
+                } // end for loop
+            } // end using scope
+        }
+    } // end class
+} // end namespace
